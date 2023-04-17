b96b28ace2b616a8dcb3a6e71c9ff8918fc11033
RubenPari <rubenpari.developer@gmail.com>
2023-04-17 08:59:12 +0200

    to fix getFilmRandomRating contr

--- src/controllers/film.ts
+++ src/controllers/film.ts
@@ -1,11 +1,77 @@
-export const getFilmRandom = async (req: any, res: any) => {
-  return "Hello, world!";
+import Hapi from "@hapi/hapi";
+import Axios, { AxiosResponse } from "axios";
+
+export const getFilmRandom = async (
+  _req: Hapi.Request,
+  _res: Hapi.ResponseToolkit
+) => {
+  console.log("Called getFilmRandom endpoint");
+
+  const movieID = Math.floor(Math.random() * 10000) + 1;
+  let response;
+
+  console.log("generate random id: " + movieID);
+
+  const url =
+    process.env.BASE_URL +
+    "/movie/" +
+    movieID +
+    "?api_key=" +
+    process.env.API_KEY;
+
+  try {
+    response = await Axios.get(url);
+  } catch (error) {
+    return error;
+  }
+
+  if (response.status !== 200) {
+    return "request doesn't have success response";
+  }
+
+  return response.data;
 };
 
 export const getFilmRandomGenre = async (req: any, res: any) => {
   return "Hello, world!";
 };
 
-export const getFilmRandomRating = async (req: any, res: any) => {
-  return "Hello, world!";
+export const getFilmRandomRating = async (
+  req: Hapi.Request,
+  res: Hapi.ResponseToolkit
+) => {
+  console.log("Called getFilmRandomRating endpoint");
+
+  console.log("The rating is " + req.params.rating);
+
+  const rating = parseInt(req.params.rating);
+
+  let movieID: number;
+  let url: string;
+  let response: AxiosResponse;
+
+  do {
+    movieID = Math.floor(Math.random() * 10000) + 1;
+
+    url =
+      process.env.BASE_URL +
+      "/movie/" +
+      movieID +
+      "?api_key=" +
+      process.env.API_KEY;
+
+    try {
+      response = await Axios.get(url);
+    } catch (error) {
+      return error;
+    }
+
+    if (response.status != 200) {
+      return "request doesn't have success response";
+    }
+
+    console.log("The vote got is " + response.data.vote_average);
+  } while (parseInt(response.data.vote_average) < rating);
+
+  return response.data;
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,7 +13,7 @@ export const routes = [
   },
   {
     method: "GET",
-    path: "/film/random/rating",
+    path: "/film/random/rating/{rating}",
     handler: getFilmRandomRating
   }
 ];
